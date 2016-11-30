f9af2f1ced012b49a3375fa4f930dc4e0a25c179
Paulo H Perbone Rocha <paulohenriquerocha@yahoo.com>
2016-11-29 22:34:50 -0200

    Add temporary SuppressWarnins unused for the default execution mode constant.

--- src/main/java/io/perbone/jejdbc/jdbc/Driver.java
+++ src/main/java/io/perbone/jejdbc/jdbc/Driver.java
@@ -55,6 +55,7 @@ public class Driver implements java.sql.Driver
 
     private static final Pattern URL_PATTERN = Pattern.compile("jdbc:jejdbc:(?://)?([^/]+)(?:/.+)?");
 
+    @SuppressWarnings("unused")
     private static final ExecutionMode DEFAULT_EXECUTION_MODE = ExecutionMode.LOCAL;
 
     /**
